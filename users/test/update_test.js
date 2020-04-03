af44c7c961069f458b982c921e6ed6434ca2e671
Akio Satoh <devtunnel@gmail.com>
2020-04-03 15:26:41 -0700

    Finished 37. Model Instance Updates

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -11,14 +11,22 @@ describe('Updating records', () => {
       .then(() => done())
   })
 
-  it('instance type using set n save', (done) => {
-    joe.set('name', 'Alex')
-    joe.save()
+  function assertName (operation, done) {
+    operation
       .then(() => User.find({}))
       .then((users) => {
         assert(users.length === 1)
         assert(users[0].name === 'Alex')
         done()
       })
+  }
+
+  it('instance type using set n save', (done) => {
+    joe.set('name', 'Alex')
+    assertName(joe.save(), done)
+  })
+
+  it('instance type using updateOne', (done) => {
+    assertName(joe.updateOne({ name: 'Alex' }), done)
   })
 })
