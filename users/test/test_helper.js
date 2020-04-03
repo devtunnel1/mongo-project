a2528b9a0eae15bac4a497d6c71ad18cf3dd030c
Akio Satoh <devtunnel@gmail.com>
2020-04-03 14:48:17 -0700

    Finished 34. More Class Based Removals

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
 mongoose.set('useUnifiedTopology', true)
 mongoose.Promise = global.Promise // ES6 Promise
 
