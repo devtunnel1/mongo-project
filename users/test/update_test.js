f1486ce04dc7b2d0e1e0662b3d6bf1fead33633e
Akio Satoh <devtunnel@gmail.com>
2020-04-06 14:35:32 -0700

    Finished 53. Fixing Update Tests

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 })
+    joe = new User({ name: 'Joe', likes: 0 })
 
     joe.save()
       .then(() => done())
@@ -42,11 +42,11 @@ describe('Updating records', () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done)
   })
 
-  xit('A user can have their postCount incremented by 1', (done) => {
-    User.updateOne({ name: 'Joe' }, { $inc: { postCount: 1 } })
+  it('A user can have their postCount incremented by 1', (done) => {
+    User.updateOne({ name: 'Joe' }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1)
+        assert(user.likes === 1)
         done()
       })
   })
